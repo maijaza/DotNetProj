1f8da22dd5e2c3d0d0ee2547587cecf6dfd4535e
sangtawee <sangtawee.techakittikajorn@ktb.co.th>
2019-09-10 18:20:24 +0700

    initial unit of work class

--- src/Api/UnitOfWork/DBContext.cs
+++ src/Api/UnitOfWork/DBContext.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Api.Repositories
+namespace Api.UnitOfWork
 {
-    public class IRepository
+    public class DBContext
     {
-         
     }
 }
---
+++ src/Api/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        DBContext db;
+        public void Commit() { }
+        public void Rollback()  {}
+
+        public UnitOfWork(DBContext db) {
+            db = db;
+        }
+        public UnitOfWork()
+        {
+            db = new DBContext();
+        }
+    }
+}
